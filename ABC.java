76c74a9d533f03e20db0242411c948d7eca4f12b
Ganesh Rajole <ganeshrajole@gmail.com>
2022-11-03 13:28:08 +0530

    Stashed File

--- ABC.java
+++ ABC.java
@@ -3,12 +3,23 @@ public class ABC {
 public static void main(Sgtring[] args) {
 
 
+<<<<<<< Updated upstream
 syso("No is Even");
 
 
 esle{
 
 syso("No is Even")
+=======
+int n=20;
+
+if(n%2==0)
+
+{
+
+
+
+>>>>>>> Stashed changes
 
 }
 
